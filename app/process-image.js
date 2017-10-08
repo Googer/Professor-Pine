95203bfdef9f19b83bcde8f73c8c6a7ca5800faa
ForcedUnknown <31086074+ForcedUnknown@users.noreply.github.com>
2017-10-08 03:16:46 -0400

    So many problems....

--- app/process-image.js
+++ app/process-image.js
@@ -6,9 +6,9 @@ const log = require('loglevel').getLogger('Raid'),
 	moment = require('moment'),
 	Helper = require('../app/helper'),
 	Jimp = require('Jimp'),
-	GymType = require('../types/gym'),
-	TimeType = require('../types/time'),
-	PokemonType = require('../types/pokemon'),
+	GymArgumentType = require('../types/gym'),
+	TimeArgumentType = require('../types/time'),
+	PokemonArgumentType = require('../types/pokemon'),
 	Raid = require('../app/raid');
 
 const debug = true;//function checkDebugFlag() { for (let arg of process.argv) { if (arg == '--debug') { return true } } return false; }();
@@ -21,7 +21,7 @@ class ImageProcess {
 	process(message, url) {
 		// easier test case
 		if (message.content == 'ping') {
-			url = path.join(__dirname, this.image_path, 'image19.png');
+			url = path.join(__dirname, this.image_path, 'image59.png');
 		}
 
 		Jimp.read(url).then((image) => {
@@ -33,9 +33,7 @@ class ImageProcess {
 			// some phones are really wierd? and have way too much height to them, and need this check to push cropping around a bit
 			const check_phone_color = Jimp.intToRGBA(image.getPixelColor(0, 85));
 
-			// location of cropping / preprocessing for different pieces of information
-			let time_remaining_a = { x: image.bitmap.width - (image.bitmap.width / 3.4), y: image.bitmap.height - (image.bitmap.height / 2.2), width: image.bitmap.width / 4, height: image.bitmap.height / 12 };
-			let time_remaining_b = { x: 0, y: image.bitmap.height / 6.4, width: image.bitmap.width, height: image.bitmap.height / 5 };
+			let gym_location = { x: image.bitmap.width / 5.1, y: image.bitmap.height / 26, width: image.bitmap.width - (image.bitmap.width / 2.55), height: image.bitmap.height / 13 };
 
 			// special case for some kind of odd vertical phone
 			if (check_phone_color.r <= 20 && check_phone_color.g <= 20 && check_phone_color.b <= 20) {
@@ -46,29 +44,24 @@ class ImageProcess {
 			return new Promise((resolve, reject) => {
 				let promises = [];
 
-				// FIRST STEP:  Determine if the screenshot is a hatched raid boss, or an egg
-				//		best way to do that (I think?) is to check where the raid time remaining is on the screen
-				this.getRaidTimeRemaining(image, time_remaining_a).then(time => {
-					console.log('A: ', time);
+				// FIRST STEP:  Determine if the screenshot has a valid gym name
+				this.getGymName(image, gym_location).then(gym => {
+					const GymType = new GymArgumentType(Helper.client);
 
-					// if first location (bottom-right) didn't contain a time, check second location (top-middle)
-					if (!time) {
-						this.getRaidTimeRemaining(image, time_remaining_b).then(time => {
-							console.log('B: ', time);
+					// ensure gym exist and is allowed to be created
+					GymType.validate(gym, message).then(validation => {
+						if (validation == true) {
+							this.getRaidData(image).then(values => {
+								values.gym = gym;
 
-							this.getRaidData(image, true).then(values => {
-								// add time remaining to list of values
-								values.push(time);
 								resolve(values);
 							});
-						});
-					} else {
-						this.getRaidData(image).then(values => {
-							// add time remaining to list of values
-							values.push(time);
-							resolve(values);
-						});
-					}
+						} else {
+							reject(validation);
+						}
+					}).catch(err => {
+						reject(err);
+					});
 				});
 			});
 		}).then(values => {
@@ -76,50 +69,62 @@ class ImageProcess {
 		}).catch(err => console.log(err));
 	}
 
-	blacken(x, y, idx) {
+	/**
+	 * Header can contain black-gray text or white-gray text
+	 *		need to turn these areas into extremes and filter out everything else
+	 **/
+	blackenHeaderContent(x, y, idx) {
 		var red   = this.bitmap.data[ idx + 0 ];
 		var green = this.bitmap.data[ idx + 1 ];
 		var blue  = this.bitmap.data[ idx + 2 ];
 		var alpha = this.bitmap.data[ idx + 3 ];
 
-		if ((red >= 190 && green >= 200 && blue >= 200) || (red <= 70 && green <= 70 && blue <= 70)) {
-			// black & white pixels should be white
-			if (red >= 190 && green >= 200 && blue >= 200) {
-				this.bitmap.data[ idx + 0 ] = 255;
-				this.bitmap.data[ idx + 1 ] = 255;
-				this.bitmap.data[ idx + 2 ] = 255;
-			} else {
-				this.bitmap.data[ idx + 0 ] = 0;
-				this.bitmap.data[ idx + 1 ] = 0;
-				this.bitmap.data[ idx + 2 ] = 0;
-			}
-		} else {
+		if ((red >= 190 && green >= 200 && blue >= 200) || (red <= 60 && green <= 60 && blue <= 60)) {
 			this.bitmap.data[ idx + 0 ] = 255;
+			this.bitmap.data[ idx + 1 ] = 255;
+			this.bitmap.data[ idx + 2 ] = 255;
+		} else {
+			this.bitmap.data[ idx + 0 ] = 0;
 			this.bitmap.data[ idx + 1 ] = 0;
 			this.bitmap.data[ idx + 2 ] = 0;
 		}
 	}
 
-	// more extreme version...
-	blacken2(x, y, idx) {
+	/**
+	 * Normal body text will always be white-gray text, don't need to be as aggressive here
+	 **/
+	blackenBodyContent(x, y, idx) {
 		var red   = this.bitmap.data[ idx + 0 ];
 		var green = this.bitmap.data[ idx + 1 ];
 		var blue  = this.bitmap.data[ idx + 2 ];
 		var alpha = this.bitmap.data[ idx + 3 ];
 
-		if ((red >= 220 && green >= 220 && blue >= 220) || (red <= 30 && green <= 30 && blue <= 30)) {
-			// black & white pixels should be white
-			if (red >= 220 && green >= 220 && blue >= 220) {
-				this.bitmap.data[ idx + 0 ] = 255;
-				this.bitmap.data[ idx + 1 ] = 255;
-				this.bitmap.data[ idx + 2 ] = 255;
-			} else {
-				this.bitmap.data[ idx + 0 ] = 0;
-				this.bitmap.data[ idx + 1 ] = 0;
-				this.bitmap.data[ idx + 2 ] = 0;
-			}
-		} else {
+		if (red >= 210 && green >= 210 && blue >= 210) {
 			this.bitmap.data[ idx + 0 ] = 255;
+			this.bitmap.data[ idx + 1 ] = 255;
+			this.bitmap.data[ idx + 2 ] = 255;
+		} else {
+			this.bitmap.data[ idx + 0 ] = 0;
+			this.bitmap.data[ idx + 1 ] = 0;
+			this.bitmap.data[ idx + 2 ] = 0;
+		}
+	}
+
+	/**
+	 * Large text such as the pokemon name, cp, or tier information is here and will always be white-gray
+	 **/
+	blackenLargeBodyContent(x, y, idx) {
+		var red   = this.bitmap.data[ idx + 0 ];
+		var green = this.bitmap.data[ idx + 1 ];
+		var blue  = this.bitmap.data[ idx + 2 ];
+		var alpha = this.bitmap.data[ idx + 3 ];
+
+		if (red >= 200 && green >= 200 && blue >= 200) {
+			this.bitmap.data[ idx + 0 ] = 255;
+			this.bitmap.data[ idx + 1 ] = 255;
+			this.bitmap.data[ idx + 2 ] = 255;
+		} else {
+			this.bitmap.data[ idx + 0 ] = 0;
 			this.bitmap.data[ idx + 1 ] = 0;
 			this.bitmap.data[ idx + 2 ] = 0;
 		}
@@ -131,7 +136,7 @@ class ImageProcess {
 			const dst1 = path.join(__dirname, this.image_path, 'cropped1a.png');
 			const dst2 = path.join(__dirname, this.image_path, 'cropped1b.png');
 
-			const width = region.width / 5;
+			const width = region.width / 4;
 
 			// checking left and right sides of image for time...
 			const region1 = { x: region.x, y: region.y, width, height: region.height };
@@ -142,7 +147,7 @@ class ImageProcess {
 			promises.push(new Promise((resolve, reject) => {
 				const new_image = image.clone()
 					.crop(region1.x, region1.y, region1.width, region1.height)
-					.scan(0, 0, region1.width, region1.height, this.blacken)
+					.scan(0, 0, region1.width, region1.height, this.blackenHeaderContent)
 					.getBuffer(Jimp.MIME_PNG, (err, image) => {
 						if (err) { reject(err); }
 
@@ -167,7 +172,7 @@ class ImageProcess {
 			promises.push(new Promise((resolve, reject) => {
 				const new_image = image.clone()
 					.crop(region2.x, region2.y, region2.width, region2.height)
-					.scan(0, 0, region2.width, region2.height, this.blacken)
+					.scan(0, 0, region2.width, region2.height, this.blackenHeaderContent)
 					.getBuffer(Jimp.MIME_PNG, (err, image) => {
 						if (err) { reject(err); }
 
@@ -198,6 +203,75 @@ class ImageProcess {
 		});
 	}
 
+	getRaidTimeRemaining(image, region) {
+		return new Promise((resolve, reject) => {
+			const dst1 = path.join(__dirname,  this.image_path, 'cropped5a.png');
+			const dst2 = path.join(__dirname,  this.image_path, 'cropped5b.png');
+
+			let region1 = { x: region.width - (region.width / 3.4), y: region.height - (region.height / 2.2), width: region.width / 4, height: region.height / 12 };
+			let region2 = { x: 0, y: region.height / 6.4, width: region.width, height: region.height / 5 };
+
+			let promises = [];
+
+			promises.push(new Promise((resolve, reject) => {
+				const new_image = image.clone()
+					.crop(region1.x, region1.y, region1.width, region1.height)
+					.scan(0, 0, region1.width, region1.height, this.blackenHeaderContent)
+					.getBuffer(Jimp.MIME_PNG, (err, image) => {
+						if (err) { reject(err); }
+
+						tesseract.create().recognize(image)
+							// .progress(message => console.log(message))
+							.catch(err => reject(err))
+							.then(result => {
+								const match = result.text.match(/[0-9]{1,2}\:[0-9]{1,2}\:[0-9]{1,2}/g);
+								if (match && match.length) {
+									resolve(match[0]);
+								} else {
+									resolve();
+								}
+							});
+					});
+
+				if (debug) {
+					new_image.write(dst1);
+				}
+			}));
+
+			promises.push(new Promise((resolve, reject) => {
+				const new_image = image.clone()
+					.crop(region2.x, region2.y, region2.width, region2.height)
+					.scan(0, 0, region2.width, region2.height, this.blackenHeaderContent)
+					.getBuffer(Jimp.MIME_PNG, (err, image) => {
+						if (err) { reject(err); }
+
+						tesseract.create().recognize(image)
+							// .progress(message => console.log(message))
+							.catch(err => reject(err))
+							.then(result => {
+								const match = result.text.match(/[0-9]{1,2}\:[0-9]{1,2}\:[0-9]{1,2}/g);
+								if (match && match.length) {
+									resolve(match[0]);
+								} else {
+									resolve();
+								}
+							});
+					});
+
+				if (debug) {
+					new_image.write(dst2);
+				}
+			}));
+
+			// pass along collected data once all promises have resolved
+			Promise.all(promises).then(values => {
+				resolve(values[0] || values[1]);
+			}).catch(err => {
+				reject(err);
+			});
+		});
+	}
+
 	getGymName(image, region) {
 		const dst = path.join(__dirname, this.image_path, 'cropped2.png');
 
@@ -205,7 +279,7 @@ class ImageProcess {
 			const new_image = image.clone()
 				.crop(region.x, region.y, region.width, region.height)
 				// .brightness(-0.1)
-				.scan(0, 0, region.width, region.height, this.blacken)
+				.scan(0, 0, region.width, region.height, this.blackenBodyContent)
 				.getBuffer(Jimp.MIME_PNG, (err, image) => {
 					if (err) { reject(err); }
 
@@ -231,7 +305,7 @@ class ImageProcess {
 				.crop(region.x, region.y, region.width, region.height)
 				.blur(3)
 				.brightness(-0.2)
-				.scan(0, 0, region.width, region.height, this.blacken)
+				.scan(0, 0, region.width, region.height, this.blackenLargeBodyContent)
 				.getBuffer(Jimp.MIME_PNG, (err, image) => {
 					if (err) { reject(err); }
 
@@ -239,7 +313,10 @@ class ImageProcess {
 						// .progress(message => console.log(message))
 						.catch(err => reject(err))
 						.then(result => {
-							resolve(result.text.replace(/(CP|cp)?\s?[0-9]*/g, '').replace(/[-!$%^&*()_+|~=`{}\[\]:"“;'<>?,.\/\n\s]/g, ''));
+							const text = result.text.replace(/[-!$%^&*()_+|~=`{}\[\]:"“”‘;'<>?,.\/]/gi, '');
+							const cp = (text.match(/[0-9]+/g) || [''])[0];
+							const pokemon = text.replace(/(cp)?\s?[0-9]*/g, '');
+							resolve([cp, pokemon]);
 						});
 				});
 
@@ -255,7 +332,7 @@ class ImageProcess {
 		return new Promise((resolve, reject) => {
 			const new_image = image.clone()
 				.crop(region.x, region.y, region.width, region.height)
-				.scan(0, 0, region.width, region.height, this.blacken2)
+				.scan(0, 0, region.width, region.height, this.blackenLargeBodyContent)
 				.blur(3)
 				.getBuffer(Jimp.MIME_PNG, (err, image) => {
 					if (err) { reject(err); }
@@ -280,40 +357,12 @@ class ImageProcess {
 		});
 	}
 
-	getRaidTimeRemaining(image, region) {
-		const dst = path.join(__dirname,  this.image_path, 'cropped5.png');
-
-		return new Promise((resolve, reject) => {
-			const new_image = image.clone()
-				.crop(region.x, region.y, region.width, region.height)
-				.scan(0, 0, region.width, region.height, this.blacken2)
-				.getBuffer(Jimp.MIME_PNG, (err, image) => {
-					if (err) { reject(err); }
-
-					tesseract.create().recognize(image)
-						.catch(err => reject(err))
-						.then(result => {
-							const match = result.text.match(/[0-9]{1,2}\:[0-9]{1,2}\:[0-9]{1,2}/g);
-							if (match && match.length) {
-								resolve(match[0]);
-							} else {
-								resolve();
-							}
-						});
-				});
-
-			if (debug) {
-				new_image.write(dst);
-			}
-		});
-	}
-
 	getRaidData(image, egg=false) {
-		// location of cropping / preprocessing for different pieces of information
+		// location of cropping / preprocessing for different pieces of information (based on % width & % height for scalability purposes)
 		let phone_time = { x: image.bitmap.width / 2.5, y: 0, width: image.bitmap.width, height: image.bitmap.height / 27 };
-		let gym_location = { x: image.bitmap.width / 5.1, y: image.bitmap.height / 26, width: image.bitmap.width - (image.bitmap.width / 2.55), height: image.bitmap.height / 13 };
 		let pokemon_name = { x: 0, y: image.bitmap.height / 6.4, width: image.bitmap.width, height: image.bitmap.height / 5 };
 		let tier = { x: 0, y: image.bitmap.height / 4.0, width: image.bitmap.width, height: image.bitmap.height / 9 };
+		let all = { x: 0, y: 0, width: image.bitmap.width, height: image.bitmap.height };
 
 		return new Promise((resolve, reject) => {
 			let promises = [];
@@ -321,39 +370,48 @@ class ImageProcess {
 			// PHONE TIME
 			promises.push(this.getPhoneTime(image, phone_time));
 
-			// GYM NAME
-			promises.push(this.getGymName(image, gym_location));
+			// TIME REMAINING
+			promises.push(this.getRaidTimeRemaining(image, all));
 
-			if (egg) {
-				// POKEMON NAME
-				promises.push(this.getTier(image, tier));
-			} else {
-				// POKEMON NAME
-				promises.push(this.getPokemonName(image, pokemon_name));
-			}
+			// POKEMON TIER
+			promises.push(this.getTier(image, tier));
+
+			// POKEMON NAME
+			promises.push(this.getPokemonName(image, pokemon_name));
 
 			// pass along collected data once all promises have resolved
 			Promise.all(promises).then(values => {
-				resolve(values);
+				console.log(values);
+
+				resolve({
+					phone_time: values[0],
+					time_remaining: values[1],
+					tier: values[2],
+					cp: values[3][0],
+					pokemon: values[3][1],
+				});
 			}).catch(err => {
 				reject(err);
 			});
 		});
 	}
 
-	createRaid(message, values) {
-		console.log(values);
-		let gym = values[1];
-		let pokemon = values[2];
-		let time;
+	createRaid(message, data) {
+		const GymType = new GymArgumentType(Helper.client);
+		const PokemonType = new PokemonArgumentType(Helper.client);
+		const TimeType = new TimeArgumentType(Helper.client);
+
+		console.log(data);
+		let pokemon = data.pokemon;
+		let time = data.time;
 
 		// if AM or PM already exists in time, use time as is
-		if (values[0].search(/(a|p)m/gi) >= 0) {
-			time = moment(values[0], 'hh:mma');
+		if (data.phone_time.search(/(a|p)m/gi) >= 0) {
+			time = moment(data.phone_time, 'hh:mma');
 		} else {
 			// else figure out if time should be AM or PM
-			const time_am = moment(values[0] + 'am', 'hh:mma');
-			const time_pm = moment(values[0] + 'pm', 'hh:mma');
+			const time_am = moment(data.phone_time + 'am', 'hh:mma');
+			const time_pm = moment(data.phone_time + 'pm', 'hh:mma');
 			const times = [ time_am.diff(moment()), time_pm.diff(moment()) ]
 			if (times[0] < times[1]) {
 				time = time_am;
@@ -362,12 +420,28 @@ class ImageProcess {
 			}
 		}
 
-		pokemon = (new PokemonType(Helper.client)).parse(pokemon, { message, client: Helper.client });
-		time = (new TimeType(Helper.client)).parse(time.format('hh:mma'), { message, client: Helper.client, argString: '' });
-		gym = (new GymType(Helper.client)).parse(gym, message).then(gym => {
+		console.log(time.format('h:mma'));
+
+		// Need to fake ArgumentType data in order to parse time...
+		message.argString = '';
+
+		if (PokemonType.validate(data.pokemon, message) == true) {
+			pokemon = PokemonType.parse(data.pokemon, message);
+		} else {
+			pokemon = PokemonType.parse('????', message);
+		}
+
+		if (TimeType.validate('at' + time.format('h:mma'), message, { prompt: '' }) == true) {
+			time = TimeType.parse('at' + time.format('h:mma'), message);
+		} else {
+			console.log(moment().format('h:mma'));
+			time = TimeType.parse('at' + moment().format('h:mma'), message);
+		}
+
+		GymType.parse(data.gym, message).then(gym => {
 			console.log(gym, pokemon, time);
 
-			if (values[0].length && values[1].length && values[2].length && values[3].length) {
+			if (pokemon && time && gym) {
 				let raid;
 
 				Raid.createRaid(message.channel.id, message.member.id, pokemon, gym, time)
@@ -395,7 +469,7 @@ class ImageProcess {
 					})
 					.catch(err => log.error(err))
 			} else {
-				channel.send(values.join('\n'));
+				channel.send(Object.values(data).join('\n'));
 			}
 		});
 	}
