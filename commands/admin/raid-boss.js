0550a7198bcfd054a46c96af57bdac4661710773
Jeremy Dentel <jeremy-dentel@users.noreply.github.com>
2018-11-27 01:53:23 -0500

    Basic skeleton command, limit to bot management roles.

--- commands/admin/raid-boss.js
+++ commands/admin/raid-boss.js
@@ -1,1 +1,49 @@
-// add management command for !raid-boss <pokemon> <tier>
+"use strict";
+
+const log = require('loglevel').getLogger('AddRaidBossCommand'),
+  Commando = require('discord.js-commando'),
+  {CommandGroup} = require('../../app/constants'),
+  Helper = require('../../app/helper'),
+  Role = require('../../app/role'),
+  settings = require('../../data/settings');
+
+class AddRaidBossCommand extends Commando.Command {
+  constructor(client) {
+    super(client, {
+      name: 'raid-boss',
+      group: CommandGroup.ADMIN,
+      memberName: 'raid-boss',
+      description: 'Adds a new raid boss.',
+      examples: ['\t!raid-boss magnemite 1'],
+      arguments: [
+        {
+          key: 'pokemon',
+          prompt: 'What pokémon are you adding?\nExample: `lugia`\n',
+          type: 'pokemon'
+        },
+        {
+          key: 'tier',
+          prompt: 'What tier is this pokémon? (`1`, `2`, `3`, `4`, `5`, `ex`)',
+          type: 'string'
+        }
+      ],
+      guildOnly: true
+    });
+
+    client.dispatcher.addInhibitor(message => {
+      if (!!message.command && message.command.name === 'raid-boss') {
+        if (!Helper.isBotManagement(message)) {
+          return ['unauthorized', message.reply('You are not authorized to use this command.')];
+        }
+      }
+
+      return false;
+    });
+  }
+
+  async run(message, args) {
+    // add to db.
+  }
+}
+
+module.exports = AddRaidBossCommand;
