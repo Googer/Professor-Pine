3c678d9bc0baaa9cbcbf3606188cdd7fd50bd113
Jeremy Dentel <jdentel91@gmail.com>
2019-02-10 15:56:25 -0500

    Add in special triggers for meet command to register an unset command, add in member notifications.

--- commands/raids/cancel-start-time.js
+++ commands/raids/cancel-start-time.js
@@ -2,7 +2,7 @@
 
 const log = require('loglevel').getLogger('CancelStartTimeCommand'),
   Commando = require('discord.js-commando'),
-  {CommandGroup} = require('../../app/constants'),
+  {CommandGroup, PartyStatus} = require('../../app/constants'),
   Helper = require('../../app/helper'),
   PartyManager = require('../../app/party-manager'),
   settings = require('../../data/settings');
@@ -16,7 +16,7 @@ class CancelStartTimeCommand extends Commando.Command {
       aliases: ['c-meet', 'cm', 'cancel-start', 'cs'],
       description: 'Cancels the planned meeting time for an existing raid.',
       details: 'Use this command to cancel when a raid group intends to do the raid.',
-      examples: ['\t!cancel-meet 2:20pm'],
+      examples: ['\t!cancel-meet'],
       guildOnly: true
     });
   }
@@ -33,7 +33,30 @@ class CancelStartTimeCommand extends Commando.Command {
 
     message.react(Helper.getEmoji(settings.emoji.thumbsUp) || '👍')
       .catch(err => log.error(err));
-    
+
+    const groupId = raid.attendees[message.member.id].group,
+      totalAttendees = raid.getAttendeeCount(groupId),
+      verb = totalAttendees === 1 ?
+        'is' :
+        'are',
+      noun = totalAttendees === 1 ?
+        'trainer' :
+        'trainers',
+      channel = (await PartyManager.getChannel(raid.channelId)).channel;
+
+    // notify all attendees in same group that a time has been set
+    Object.entries(raid.attendees)
+      .filter(([attendee, attendeeStatus]) => attendee !== message.member.id &&
+        attendeeStatus.status !== PartyStatus.COMPLETE)
+      .filter(([attendee, attendeeStatus]) => attendeeStatus.group === groupId)
+      .forEach(([attendee, attendeeStatus]) => {
+        const member = Helper.getMemberForNotification(message.guild.id, attendee);
+
+        member.send(`${message.member.displayName} has canceled the meeting time for ${channel.toString()}. ` +
+          `There ${verb} currently **${totalAttendees}** ${noun} attending!`)
+          .catch(err => log.error(err));
+      });
+
     raid.refreshStatusMessages();
   }
 }
