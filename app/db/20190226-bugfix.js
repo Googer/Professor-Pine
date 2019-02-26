288e54cc8a7f2e62283d660680704dfff87e109b
Jeremy Dentel <jdentel91@gmail.com>
2019-02-26 18:44:43 -0500

    Finish fix for Zero and Perfects.

---
+++ app/db/20190226-bugfix.js
@@ -0,0 +1,29 @@
+const DB = require('../db');
+
+exports.up = async function (knex, Promise) {
+  let promises = [],
+    users = await DB.DB('PokemonNotification')
+      .where('pokemon', -7);
+
+  users.forEach(notification => {
+    let newNotification = {
+      pokemon: -6,
+      guildId: notification.guildId,
+      userId: notification.userId,
+      type: notification.type
+    };
+
+    promises.push(DB.DB('PokemonNotification').insert(newNotification));
+    promises.push(DB.DB('PokemonNotification')
+      .where('id', notification.id)
+      .update({
+        pokemon: -8
+      }));
+  });
+
+  return Promise.all(promises);
+};
+
+exports.down = function (knex, Promise) {
+  return Promise.all([]);
+};
