5ce039b2ab9ffe45229c2cef1aed30b6c7b2cbf0
ExileExo <47049336+ExileExo@users.noreply.github.com>
2019-07-19 16:59:33 -0400

    Update pvp-rank.js

--- commands/util/pvp-rank.js
+++ commands/util/pvp-rank.js
@@ -1,10 +1,10 @@
-const log = require('loglevel').getLogger('Counters'),
+const log = require('loglevel').getLogger('RankCommand'),
   Commando = require('discord.js-commando'),
   {MessageEmbed} = require('discord.js'),
   {CommandGroup} = require('../../app/constants'),
   fetch = require('node-fetch'),
   cheerio = require('cheerio'),
-  Utility = require('../../app/utility');
+  Helper = require('../../app/helper');
 
 class PvPRankingData {
   constructor(pokemon, league, atk, def, sta, filter){
@@ -25,14 +25,8 @@ class PvPRankingData {
     String.prototype.titleCase = function () {
       return this.replace(/\w\S*/g, function(str) {return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();});
     };
-    this.cpLeague;
     let ivFilter;
-    if(this.inputLeague == 'ultra'){
-      this.cpLeague = "2500";
-    }
-    else {
-      this.cpLeague = "1500";
-    }
+    this.cpLeague = this.inputLeague == 'ultra' ? "2500" : "1500";
     if(this.inputFilter != "10"){
      ivFilter = "0"
     }
@@ -50,12 +44,11 @@ class PvPRankingData {
       .catch(err => log.error(err));
     let ivScrapeData = [];
     let siteData;
+    //let ivScrapeData = $('tr[class^="table-"]').children().map((i, el) => $(el).text()).get();    <-- this code replaces the function below and works. idk how. Magic i think.
     $("tr[class^=\"table-\"]").children().each(function(){
       siteData = $(this).text();
       ivScrapeData.push(siteData);
-      //console.log(typeof(siteData), siteData)
     })
-    console.log(typeof(this.inputPokemon), this.inputPokemon.pbName),
     this.name = this.inputPokemon.titleCase();
     this.rank = ivScrapeData[0];
     this.level = ivScrapeData[1];
@@ -80,63 +73,72 @@ class PvPRankingData {
     }
     let requestInfo = `\n**${league} LEAGUE\nRank**: ${this.rank} (${this.statproduct})\n` +
     `**CP**: ${this.cp} @ Level ${this.level}\n`;
-    let nameField = `**${args['pvp_pokemon'].pbName.titleCase()}**  ${this.inputATKiv}/${this.inputDEFiv}/${this.inputSTAiv}\n`; 
+    let aestheticPokemonName1 = args['pvp_pokemon'].aliases[1];
+    let nameField = `**${aestheticPokemonName1.titleCase()}**  ${this.inputATKiv}/${this.inputDEFiv}/${this.inputSTAiv}\n`; 
     const embed = new MessageEmbed()
     .setColor('#43B581')
     .addField(nameField, requestInfo)
     .setThumbnail(args['pvp_pokemon'].imageURL)
     .setFooter(`${message.author.username} || Data retrieved from https://gostadium.club`);
 
+    let aestheticPokemonName2 = args['pvp_pokemon'].aliases[args['pvp_pokemon'].aliases.length-1];
     let userCommand = `${message.client.commandPrefix}${command}`; //Grabs the !great, !ultra or w/e from user inputs.
-    let userPokemon = `${args['pvp_pokemon'].pbName.titleCase()}`; //Grabs the accepted pokemon name.
+    let userPokemon = `${aestheticPokemonName2.replace(/ /g,"-").toLowerCase()}`; //Grabs the accepted Pokémon name.
     let userIVString = `${this.inputATKiv} ${this.inputDEFiv} ${this.inputSTAiv}`; //Grabs the accepted IV sets (0-15 for ATK,DEF,STA)
-    let responseCommand = `${message.author}, here are your \`${userCommand} ${userPokemon} ${userIVString}\` results:`; //Combined the above into the whole accepted command.
+    let responseCommand = /*`${message.author}, here are your */`\`${userCommand} ${userPokemon} ${userIVString}\` results:`; //Combined the above into the whole accepted command.
     let response = await message.channel.send(responseCommand, embed);
     response.preserve = true;
   }
 }
   class RankCommand extends Commando.Command {
     constructor(client) {
-    super(client, {
-      name: 'rank',
-      group: CommandGroup.UTIL,
-      memberName: 'rank',
-      aliases: [/*'pvp-rank', 'pvp-iv', */'great', 'ultra'], //Aliases are other user commands that call this command file
-      description: 'Provides PvP data based on a pokemon species\'s IVs, including rank and CP.', //Help info
-      details: 'Use this command in #rank-checker to obtain information on the PvP ranking of a specific IV combination for a specific species of pokemon.' +
-          '\n!great - This command restricts results to **Great League**\n!ultra - This command restricts results to **Ultra League**', //Help info
-      examples: ['!<league> <pokemon> <Attack IV> <Defense IV> <Stamina IV>\n!great altaria 4 1 2\n!ultra blastoise 10 14 15\n'], //Help info
-      guarded: false, //????
-      args: [
-        {
-          key: 'pvp_pokemon', //Variable pokemon is the first entry in the command
-          prompt: 'Which species of pokemon would you like to evaluate a specific IV combination PvP rank for?',
-          type: 'counterpokemontype', //Ensures that the entered information is a pokemon name.
-        },
-        {
-          key: 'ATK_IV',
-          prompt: 'Attack IV?',
-          type: 'integer',
-          min: 0,
-          max: 15,
-        },
-        {
-          key: 'DEF_IV',
-          prompt: 'Defense IV?',
-          type: 'integer',
-          min: 0,
-          max: 15,
-        },
-        {
-          key: 'STA_IV',
-          prompt: 'Stamina IV?',
-          type: 'integer',
-          min: 0,
-          max: 15, //Can we add additional args for optional additional parameters for specific searches? For example, minimum IV
-        },],
-      argsPromptLimit: 3, //What is this?
-    });
-  }
+      super(client, {
+        name: 'rank',
+        group: CommandGroup.UTIL,
+        memberName: 'rank',
+        aliases: [/*'pvp-rank', 'pvp-iv', */'great', 'ultra'],
+        description: 'Provides PvP data based on a Pokémon species\'s IVs, including rank and CP.', 
+        details: 'Use this command to obtain information on the PvP ranking of a specific IV combination for a specific species of Pokémon.' +
+            '\n!great - This command restricts results to **Great League**\n!ultra - This command restricts results to **Ultra League**',
+        examples: ['!<league> <Pokémon> <Attack IV> <Defense IV> <Stamina IV>\n!great altaria 4 1 2\n!ultra blastoise 10 14 15\n'], 
+        guarded: false, 
+        args: [
+          {
+            key: 'pvp_pokemon',
+            prompt: 'Which species of Pokémon would you like to evaluate a specific IV combination PvP rank for?',
+            type: 'counterpokemontype', 
+          },
+          {
+            key: 'ATK_IV',
+            prompt: 'Attack IV?',
+            type: 'integer',
+            min: 0,
+            max: 15,
+          },
+          {
+            key: 'DEF_IV',
+            prompt: 'Defense IV?',
+            type: 'integer',
+            min: 0,
+            max: 15,
+          },
+          {
+            key: 'STA_IV',
+            prompt: 'Stamina IV?',
+            type: 'integer',
+            min: 0,
+            max: 15, //Can we add additional args for optional additional parameters for specific searches? For example, minimum IV
+          },],
+        argsPromptLimit: 3, 
+      });
+      client.dispatcher.addInhibitor(message => {
+        if (!!message.command && message.command.name === 'rank' &&
+          !Helper.isPvPCategory(message)) {
+          return ['invalid-channel', message.reply(Helper.getText('pvp-rank.warning', message))];
+        }
+        return false;
+      });
+  } 
 
   async run(message, args) {
 
@@ -144,7 +146,7 @@ class PvPRankingData {
       return this.replace(/\w\S*/g, function(str) {return str.charAt(0).toUpperCase() + str.substr(1).toLowerCase();});
     };
 
-    let userCommand = message.content.match(RegExp(`^\\s*${message.client.commandPrefix}\\s*(\\S+)\\s*`))[1].toLowerCase();
+    let userCommand = message.content.substring(1).trim().split(' ').shift().toLowerCase();
     if(userCommand == 'great' || userCommand == 'rank'){
       let greatRank = new PvPRankingData(args['pvp_pokemon'].pbName,'great',args['ATK_IV'],args['DEF_IV'],args['STA_IV']);
       await greatRank.scrape();
