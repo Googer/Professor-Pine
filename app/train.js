ab1474933a7861c4b37a82f8ac71e3988c2ba622
Matt Legowski <googer@pobox.com>
2020-03-22 11:42:48 -0400

    Fix train channel positioning upon creation

--- app/train.js
+++ app/train.js
@@ -65,7 +65,7 @@ class RaidTrain extends Party {
         // move channel to end
         return newChannel.guild.setChannelPositions([{
           channel: newChannel,
-          position: newChannel.guild.channels.size - 1
+          position: newChannel.guild.channels.cache.size - 1
         }]);
       })
       .then(async guild => {
